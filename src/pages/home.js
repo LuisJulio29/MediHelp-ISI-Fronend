e683814e0e67f2d31a8b877349f62af1db46da15
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2024-11-26 15:38:22 -0500

    add search in the user home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CustomLayout from "../components/Layout";
-import { Col, Row, Tabs } from "antd";
+import { Col, Row, Tabs, Input } from "antd";
 import Doctor from "../components/Doctor";
 import { useDispatch } from "react-redux";
 import { hideloading, showloading } from "../redux/alertsSlice";
 
 function Home() {
   const [doctors, setDoctors] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -31,59 +32,95 @@ function Home() {
     getDoctors();
   }, [dispatch]);
 
-  // Función para filtrar doctores por sede
   const filterDoctorsByDepartment = (department) => {
-    return doctors.filter((doctor) => doctor.department === department);
+    return doctors
+      .filter(
+        (doctor) =>
+          doctor.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          doctor.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .filter((doctor) => doctor.department === department);
   };
+
+  const tabsItems = [
+    {
+      label: "La Merced",
+      key: "1",
+      children: (
+        <Row gutter={[20, 20]}>
+          {filterDoctorsByDepartment("La Merced").map((doctor) => (
+            <Col key={doctor._id} span={8} xs={24} sm={12} md={8} lg={6}>
+              <Doctor doctor={doctor} />
+            </Col>
+          ))}
+        </Row>
+      ),
+    },
+    {
+      label: "Piedra de Bolívar",
+      key: "2",
+      children: (
+        <Row gutter={[20, 20]}>
+          {filterDoctorsByDepartment("Piedra De Bolivar").map((doctor) => (
+            <Col key={doctor._id} span={8} xs={24} sm={12} md={8} lg={6}>
+              <Doctor doctor={doctor} />
+            </Col>
+          ))}
+        </Row>
+      ),
+    },
+    {
+      label: "San Agustín",
+      key: "3",
+      children: (
+        <Row gutter={[20, 20]}>
+          {filterDoctorsByDepartment("San Agustin").map((doctor) => (
+            <Col key={doctor._id} span={8} xs={24} sm={12} md={8} lg={6}>
+              <Doctor doctor={doctor} />
+            </Col>
+          ))}
+        </Row>
+      ),
+    },
+    {
+      label: "San Pablo",
+      key: "4",
+      children: (
+        <Row gutter={[20, 20]}>
+          {filterDoctorsByDepartment("San Pablo").map((doctor) => (
+            <Col key={doctor._id} span={8} xs={24} sm={12} md={8} lg={6}>
+              <Doctor doctor={doctor} />
+            </Col>
+          ))}
+        </Row>
+      ),
+    },
+  ];
 
   return (
     <CustomLayout>
       <div className="container">
-      <h1 className="text-center mb-4">Reservar Citas </h1>
-        {/* Pestañas para las sedes */}
-        <Tabs type="card" centered>
-          <Tabs.TabPane tab="La Merced" key="1">
-            <Row gutter={[20, 20]}>
-              {filterDoctorsByDepartment("La Merced").map((doctor) => (
-                <Col key={doctor._id} span={8} xs={24} sm={12} md={8} lg={6}>
-                  <Doctor doctor={doctor} />
-                </Col>
-              ))}
-            </Row>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Piedra de Bolívar" key="2">
-            <Row gutter={[20, 20]}>
-              {filterDoctorsByDepartment("Piedra De Bolivar").map((doctor) => (
-                <Col key={doctor._id} span={8} xs={24} sm={12} md={8} lg={6}>
-                  <Doctor doctor={doctor} />
-                </Col>
-              ))}
-            </Row>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="San Agustín" key="3">
-            <Row gutter={[20, 20]}>
-              {filterDoctorsByDepartment("San Agustin").map((doctor) => (
-                <Col key={doctor._id} span={8} xs={24} sm={12} md={8} lg={6}>
-                  <Doctor doctor={doctor} />
-                </Col>
-              ))}
-            </Row>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="San Pablo" key="4">
-            <Row gutter={[20, 20]}>
-              {filterDoctorsByDepartment("San Pablo").map((doctor) => (
-                <Col key={doctor._id} span={8} xs={24} sm={12} md={8} lg={6}>
-                  <Doctor doctor={doctor} />
-                </Col>
-              ))}
-            </Row>
-          </Tabs.TabPane>
-        </Tabs>
+        <h1 className="text-center mb-4">Reservar Citas</h1>
+        <div style={{ marginBottom: "20px", textAlign: "center" }}>
+          <Input
+            placeholder="Buscar doctor por nombre o apellido"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            style={{ maxWidth: "440px", width: "100%" }}
+          />
+        </div>
+        <div className="tabs-container">
+          <Tabs
+            type="card"
+            centered
+            items={tabsItems}
+            tabPosition= "top"
+          />
+        </div>
       </div>
     </CustomLayout>
   );
 }
 
 export default Home;
-
 
